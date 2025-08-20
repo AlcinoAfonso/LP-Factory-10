c5d3478408e6633f86f26217eeff4e4611ba160a
AlcinoAfonso <alcinoafonso@unicodigital.com.br>
2025-08-20 18:27:21 -0300

    Update page.tsx

--- app/auth/reset/page.tsx
+++ app/auth/reset/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
-import { supabase } from "@/lib/supabase-browser";
+import { supabase } from "@/lib/supabase/client";
 
 export default function ResetPasswordPage() {
   const [pwd1, setPwd1] = useState("");
