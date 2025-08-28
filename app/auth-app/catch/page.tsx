063b33bce9b6d3d6712ff44cb31e7bea70d1677c
AlcinoAfonso <alcinoafonso@unicodigital.com.br>
2025-08-27 21:01:10 -0300

    chore: rollback to commit 1b436be

--- app/auth-app/catch/page.tsx
+++
@@ -1,6 +0,0 @@
-"use client"
-
-// TODO: Implementar normalizador de token (code, token_hash, access_token)
-export default function CatchPage() {
-  return <div>Normalizador de token (sub-app auth)</div>
-}
