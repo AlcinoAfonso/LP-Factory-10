527d2a18eb1706e27d00f48b2cb40fd49668ec1b
AlcinoAfonso <alcinoafonso@unicodigital.com.br>
2025-09-02 15:36:41 -0300

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,33 +1,33 @@
-1import { createServerClient } from '@supabase/ssr'
-2import { cookies } from 'next/headers'
-3
-4/**
-5 * If using Fluid compute: Don't put this client in a global variable. Always create a new client within each
-6 * function when using it.
-7 */
-8export async function createClient() {
-9  const cookieStore = await cookies()
-10
-11  return createServerClient(
-12    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-13    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY!,
-14    {
-15      cookies: {
-16        getAll() {
-17          return cookieStore.getAll()
-18        },
-19        setAll(cookiesToSet) {
-20          try {
-21            cookiesToSet.forEach(({ name, value, options }) =>
-22              cookieStore.set(name, value, options)
-23            )
-24          } catch {
-25            // The `setAll` method was called from a Server Component.
-26            // This can be ignored if you have middleware refreshing
-27            // user sessions.
-28          }
-29        },
-30      },
-31    }
-32  )
-33}
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+/**
+ * If using Fluid compute: Don't put this client in a global variable. Always create a new client within each
+ * function when using it.
+ */
+export async function createClient() {
+  const cookieStore = await cookies()
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_OR_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  )
+}
