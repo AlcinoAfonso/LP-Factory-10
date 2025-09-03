2af206f2c3895462f71c0b89fb1da4461309ee69
AlcinoAfonso <alcinoafonso@unicodigital.com.br>
2025-09-03 20:39:13 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,13 @@
 import { updateSession } from '@/lib/supabase/middleware'
 import type { NextRequest } from 'next/server'
 
-
 export async function middleware(request: NextRequest) {
-return await updateSession(request)
+  return await updateSession(request)
 }
 
-
 export const config = {
-matcher: [
-// Aplica o middleware em tudo, exceto estáticos/imagens/favicon **e** /a/home (público)
-'/((?!a/home|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-],
+  matcher: [
+    // Protege tudo, exceto estáticos/imagens/favicon e "/a/home" (página pública/Onboarding)
+    '/((?!a/home|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 }
