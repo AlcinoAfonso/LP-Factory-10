cf3964afdb751b2e7f41d46ec84fd6d89eeb2f86
AlcinoAfonso <alcinoafonso@unicodigital.com.br>
2025-09-11 19:21:00 -0300

    Update page.tsx

--- app/a/home/page.tsx
+++ app/a/home/page.tsx
@@ -1,7 +1,7 @@
 // app/a/home/page.tsx
 import { redirect } from "next/navigation";
 
-export default function AHomeAlias() {
+export default async function AHomeAlias() {
   // Compat: redireciona chamadas antigas de /a/home para o índice /a
   redirect("/a");
 }
