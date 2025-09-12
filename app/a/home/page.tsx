5e3df0576b1bb1b6157f303ff6c30b2d8c2b0e52
AlcinoAfonso <alcinoafonso@unicodigital.com.br>
2025-09-12 18:36:31 -0300

    Delete app/a/home/page.tsx

--- app/a/home/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/a/home/page.tsx
-export const dynamic = 'force-dynamic';
-
-import { redirect } from "next/navigation";
-
-export default function AHomeAlias() {
-  // Compat: força redirecionamento SSR do legado /a/home para a landing pública /a
-  redirect("/a"); // 307
-}
