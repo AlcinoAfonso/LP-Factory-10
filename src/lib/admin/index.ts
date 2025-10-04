b48b449bdc602380e7440603d5dfe2e5663b75ad
AlcinoAfonso <alcinoafonso@unicodigital.com.br>
2025-10-04 12:39:45 -0300

    Create src/lib/admin/index.ts

---
+++ src/lib/admin/index.ts
@@ -0,0 +1,9 @@
+// src/lib/admin/index.ts
+/**
+ * Módulo Admin (E7)
+ * Exports centralizados
+ */
+
+export * from "./contracts";
+export { tokens as adminTokens } from "./adapters/adminAdapter";
+export * as postSaleTokenAdapter from "./adapters/postSaleTokenAdapter";
