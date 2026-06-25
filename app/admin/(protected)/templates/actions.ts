65369b23181cda89e5fe3f3f9d21f64959628efb
AlcinoAfonso <alcinoafonso@unicodigital.com.br>
2026-06-24 22:08:23 -0300

    Fix admin template draft UUID validation

--- app/admin/(protected)/templates/actions.ts
+++ app/admin/(protected)/templates/actions.ts
@@ -134,7 +134,7 @@ export async function publishCommercialActivationDraftAction(formData: FormData)
 }
 
 function isUuid(value: string) {
-  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{12}$/i.test(
+  return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
     value,
   );
 }
